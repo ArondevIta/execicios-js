3dd2524ec0a8a6f30c987f2dc1f9a591dad2ab26
ArondevIta <aronamaral@gmail.com>
2020-04-25 11:58:02 -0300

    ex7

---
+++ selectop.js
@@ -0,0 +1,29 @@
+/*
+Uma função é chamada da seguinte forma:
+calculadora(10, '+', 20)
+crie o corpo da função de forma que ela realize as 4 operações aritméticas
+*/
+let num1, num2, op
+num1 = prompt('digite o primeiro valor ')
+num2 = prompt('digite o segundo valor')
+op = prompt('digite qual operação deseja, /, *, +, -')
+
+calculadora = (num1, op, num2) => {
+  if(op==='*'){
+    return num1*num2
+  }else if(op==='/'){
+    if(num1 == 0 || num2 == 0){
+      return 'Não é possivel dividir por 0'
+    }else{
+      return num1/num2
+    }
+  }else if(op === '-'){
+    return num1-num2
+  }else if(op === '+'){
+    return num1+num2
+  }else{
+    return 'Operação invalida'
+  }
+}
+
+console.log(calculadora(num1, op, num2))
